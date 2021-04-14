9c45d0814221274e5015ae3f67f750796444722e
Demon0312 <62284365+Demon0312@users.noreply.github.com>
2021-04-14 20:08:38 +0800

    Update options.py

--- options.py
+++ options.py
@@ -9,7 +9,7 @@ def get_options(args=None):
         description="Attention based model for solving the Travelling Salesman Problem with Reinforcement Learning")
 
     # Data
-    parser.add_argument('--problem', default='pdp', help="The problem to solve, default 'tsp'")
+    parser.add_argument('--problem', default='pdp', help="The problem to solve, default 'pdp'")
     parser.add_argument('--graph_size', type=int, default=20, help="The size of the problem graph")
     parser.add_argument('--batch_size', type=int, default=512, help='Number of instances per batch during training')
     parser.add_argument('--epoch_size', type=int, default=1280000, help='Number of instances per epoch during training')
